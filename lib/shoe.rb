034cade4631b5de765b517cb17f7ab9512d451f9
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-23 17:29:35 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,13 +7,12 @@ class Shoe
 
   def initialize(brands)
     @brands = brands
+    if 
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
-  de
 
 end
